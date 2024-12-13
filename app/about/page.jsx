3b075b4db3a2b79bb4fd5ac757a307f59969186c
Marco Gallen <marco.gallen@gmail.com>
2024-12-13 15:22:37 +1100

    Added about page and activated forms

---
+++ app/about/page.jsx
@@ -0,0 +1,7 @@
+export default async function Page() {
+    return (
+        <main className="flex flex-col gap-8 sm:gap-16">
+            <section className="flex flex-col items-start gap-3 sm:gap-4">Hello world</section>
+        </main>
+    );
+}
